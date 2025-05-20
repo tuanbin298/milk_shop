f1d16088025db4402f5ad60a86d3011cb4bf68e1
tuanbin298 <tuannase171419@fpt.edu.vn>
2025-05-21 01:19:49 +0700

    build router

---
+++ milk_fe/src/MainLayout.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+
+// Page
+import LoginPage from "./page/LoginPage/LoginPage";
+import RegisterPage from "./page/RegisterPage/RegisterPage";
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
+
+function MainLayout() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
